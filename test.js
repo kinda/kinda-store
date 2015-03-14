bfb3e3f21e945156680b7122d74e9c47dfc83ef0
Manuel Vila <mvila@3base.com>
2015-03-14 10:22:44 +0800

    add test

---
+++ test.js
@@ -0,0 +1,11 @@
+"use strict";
+
+require('co-mocha');
+var assert = require('chai').assert;
+var KindaStore = require('./');
+
+suite('KindaStore', function() {
+  test('test', function *() {
+    assert(true);
+  });
+});
