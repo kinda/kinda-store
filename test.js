895af807187bc5f91976a0a4a2c816fdea8df6a7
Manuel Vila <mvila@3base.com>
2015-03-14 10:53:31 +0800

    add 'put and get' test

--- test.js
+++ test.js
@@ -5,7 +5,16 @@ var assert = require('chai').assert;
 var KindaStore = require('./');
 
 suite('KindaStore', function() {
-  test('test', function *() {
-    assert(true);
+  var store = KindaStore.create('mysql://localhost/test');
+
+  suiteTeardown(function *() {
+    yield store.delRange();
+  });
+
+  test('put and get', function *() {
+    var key = ['users', 'mvila'];
+    yield store.put(key, { firstName: 'Manu', age: 42 });
+    var user = yield store.get(key);
+    assert.deepEqual(user, { firstName: 'Manu', age: 42 });
   });
 });
