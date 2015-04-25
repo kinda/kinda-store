c174991f36be22fcb792d65fd43bb2de83250314
Manuel Vila <mvila@3base.com>
2015-04-25 10:11:27 +0800

    use renamed modules 'kinda-mysql-store' and 'kinda-web-sql-store'

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ var Factory = {
     var protocol = url.substr(0, pos);
     switch (protocol) {
     case 'mysql':
-      return require('kinda-store-mysql' + '').create(url, options);
+      return require('kinda-mysql-store').create(url, options);
     case 'websql':
-      return require('kinda-store-web-sql').create(url, options);
+      return require('kinda-web-sql-store').create(url, options);
     default:
       throw new Error('unknown protocol');
     }
